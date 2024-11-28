9f019c81d08584ec2b1089cee75b9b6b4b115289
conradstrassburger <98957100+conradstrassburger@users.noreply.github.com>
2024-11-28 15:16:17 +0100

    Remove routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {TimeComponent} from './time/time.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
 export class AppComponent {
   title = 'OneXTimer';
 }
