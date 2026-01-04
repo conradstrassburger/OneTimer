9241ac23db8c5819caa6566a20809169ff2d99a8
conradstrassburger <98957100+conradstrassburger@users.noreply.github.com>
2026-01-04 18:20:40 +0100

    Add number rolling animation

--- src/app/time/digit.component.ts
+++ src/app/time/digit.component.ts
@@ -1,39 +1,79 @@
 import { Component, OnInit, input } from '@angular/core';
+import { DateTime } from 'luxon';
 
 @Component({
     selector: 'digit',
     template: `
-    <span [class]="class">{{ [value] }}</span>
-  `,
+    <span [class]="class" [attr.data-next]="nextValue" [attr.data-current]="value"></span>
+    `,
     standalone: true,
     styles: `
-    span {
-      transition: opacity 200ms ease;
+    span::after {
+      display: inline-block;
+      content: attr(data-current);
+      opacity: 1;
     }
-    .fade {
-      opacity: 0.4
+    span::before {
+      position: absolute;
+      display: inline-block;
+      content: "";
+    }
+    .fade::after {
+        animation: fadeOut 450ms;
+    }
+    .fade::before {
+        animation: fadeIn 450ms;
+        content: attr(data-next);
+    }
+    @keyframes fadeIn {
+        0% {
+            transform: translateY(-25vw) rotateX(-0.25turn);
+            opacity: 0;
+        }
+        100% {
+            transform: translateY(0) rotateX(0);
+            opacity: 1;
+        }
+    }
+    @keyframes fadeOut {
+        0% {
+            transform: translateY(0);
+            opacity: 1;
+        }
+        50% {
+            opacity: 0;
+        }
+        100% {
+            transform: translateY(20vw);
+            opacity: 0;
+        }
     }
     `,
 })
 
 export class DigitComponent implements OnInit {
-    currentValue = input.required<string>();
+    digitFunction = input.required<(dt: DateTime) => number>();
     class = ""
-    value = ""
+    value: number = 0
+    nextValue: number = 0
 
     ngOnInit() {
-        this.timeDisplay()
+        this.value = this.digitFunction().call(this, DateTime.now())
         setInterval(this.timeDisplay.bind(this), 1000);
     }
 
     timeDisplay() {
-        const time = this.currentValue()
-        if (time === this.value) return;
-        this.value = time
+        const time = this.digitFunction().call(this, DateTime.now())
 
+        if (time === this.value) return;
+
+        this.nextValue = (this.value + 1) % 10
+        
         this.class = "fade"
+        
         setTimeout(() => {
             this.class = ""
-        }, 200)
+            this.value = time
+        }, 400)
     }
 }
--- src/app/time/time.component.ts
+++ src/app/time/time.component.ts
@@ -1,18 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { DateTime } from 'luxon';
 import { DigitComponent } from "./digit.component";
 
 @Component({
   selector: 'time-display',
   template: `
-    <digit [currentValue]="time.toFormat('HH').slice(0, 1)" />
-    <digit [currentValue]="time.toFormat('HH').slice(1)" />
+    <digit [digitFunction]="hour0" />
+    <digit [digitFunction]="hour1" />
     <span>:</span>
-    <digit [currentValue]="time.toFormat('mm').slice(0, 1)" />
-    <digit [currentValue]="time.toFormat('mm').slice(1)" />
+    <digit [digitFunction]="minute0" />
+    <digit [digitFunction]="minute1" />
     <span>:</span>
-    <digit [currentValue]="time.toFormat('ss').slice(0, 1)" />
-    <digit [currentValue]="time.toFormat('ss').slice(1)" />
+    <digit [digitFunction]="second0" />
+    <digit [digitFunction]="second1" />
   `,
   standalone: true,
   styles: `
@@ -21,21 +21,17 @@ import { DigitComponent } from "./digit.component";
       font-family: "Courier New", monospace;
       font-size: 20vw;
       transition: color 400ms linear;
+      overflow: hidden;
       }
     `,
   imports: [DigitComponent],
 })
 
-export class TimeComponent implements OnInit {
-  time = DateTime.now()
-  protected readonly DateTime = DateTime;
-
-  ngOnInit() {
-    this.timeDisplay()
-    setInterval(this.timeDisplay.bind(this), 1000);
-  }
-
-  timeDisplay() {
-    this.time = DateTime.now()
-  }
+export class TimeComponent {
+  hour0 = (dt: DateTime) => parseInt(dt.toFormat('HH').slice(0, 1))
+  hour1 = (dt: DateTime) => parseInt(dt.toFormat('HH').slice(1))
+  minute0 = (dt: DateTime) => parseInt(dt.toFormat('mm').slice(0, 1))
+  minute1 = (dt: DateTime) => parseInt(dt.toFormat('mm').slice(1))
+  second0 = (dt: DateTime) => parseInt(dt.toFormat('ss').slice(0, 1))
+  second1 = (dt: DateTime) => parseInt(dt.toFormat('ss').slice(1))
 }
